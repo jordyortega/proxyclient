4613e177a4abfd059169fce8cfd4146a02d6db8c
playerowns <pwned901@gmail.com>
2024-08-29 03:27:51 +0300

    Moving files to com.proxy directory + refactoring of client.java methods

---
+++ src/com/proxy/DummyClass.java
@@ -0,0 +1,13 @@
+package com.proxy;// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+
+
+public final class DummyClass {
+
+	public DummyClass()
+	{
+	}
+
+	public static DummyClass cache[];
+}
